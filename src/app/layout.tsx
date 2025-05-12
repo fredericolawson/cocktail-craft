5e6bb514711ce612e31c79dff7e121f2bc428bcb
Frederick Lawson <freddie@corneliajames.com>
2025-05-12 16:27:27 -0300

    1st

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,21 +2,20 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 
-
 export const metadata: Metadata = {
   title: "Beam",
   description: "Digital Services for Bermuda",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
+      <body className="flex flex-col">
+        <div className="min-h-screen bg-gray-100/50 flex-1 flex flex-col mx-auto w-full">
+          <main className="flex-1 flex flex-col items-center justify-center max-w-screen-2xl mx-auto w-full">
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1>Beam</h1>
-      <p>Digital Services for Bermuda</p>
+      <div className="flex flex-col items-center justify-center h-full mb-18">
+        <h1 className="text-4xl font-bold">Beam</h1>
+        <p className="text-lg">Digital Services for Bermuda</p>
+      </div>
+      <h2 className="text-xl font-bold mb-2">Links</h2>
+      <ul>
+        <li>
+          <Link href="https://taxis.beam.bm/">Beam Taxis</Link>
+        </li>
+      </ul>
     </div>
+
   );
 }
